94b567ff0adbbc198c246c9d97073f9ba7290f5b
Alexey Efimov <lex.efimov@gmail.com>
2018-07-25 10:12:47 +0500

    End of course

--- Notifications/NotificationContent/NotificationViewController.swift
+++ Notifications/NotificationContent/NotificationViewController.swift
@@ -2,7 +2,7 @@
 //  NotificationViewController.swift
 //  NotificationContent
 //
-//  Created by Alexey Efimov on 12.07.2018.
+//  Created by Alexey Efimov on 16.07.2018.
 //  Copyright © 2018 Alexey Efimov. All rights reserved.
 //
 
@@ -12,7 +12,7 @@ import UserNotificationsUI
 
 class NotificationViewController: UIViewController, UNNotificationContentExtension {
 
-    @IBOutlet weak var label: UILabel?
+    @IBOutlet var label: UILabel?
     @IBOutlet weak var likeButton: UIButton!
     
     override func viewDidLoad() {
@@ -21,13 +21,12 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
     }
     
     @IBAction func likeButtonTapped(_ sender: Any) {
-        likeButton.setTitle("♥", for: .normal)
+        likeButton.setTitle("♥︎", for: .normal)
     }
     
     @IBAction func openAppButton(_ sender: Any) {
         openApp()
     }
-    
     
     func openApp() {
         extensionContext?.performNotificationDefaultAction()
@@ -42,21 +41,23 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
     }
     
     func didReceive(_ response: UNNotificationResponse, completionHandler completion: @escaping (UNNotificationContentExtensionResponseOption) -> Void) {
+        
         switch response.actionIdentifier {
         case "Snooze":
             let actions = [
-                UNNotificationAction(identifier: "5 second",  title: "Отложить на 5 секунд", options: []),
-                UNNotificationAction(identifier: "10 second",  title: "Отложить на 10 секунд", options: []),
-                UNNotificationAction(identifier: "60 second",  title: "Отложить на 60 секунд", options: []),
-                ]
+            UNNotificationAction(identifier: "5 seconds", title: "Отложить на 5 секунд", options: []),
+            UNNotificationAction(identifier: "10 seconds", title: "Отложить на 10 секунд", options: []),
+            UNNotificationAction(identifier: "60 seconds", title: "Отложить на 60 секунд", options: [])]
+            
             extensionContext?.notificationActions = actions
-        case "5 second":
+            
+        case "5 seconds":
             reminder(timeInterval: 5)
             dismissNotification()
-        case "10 second":
+        case "10 seconds":
             reminder(timeInterval: 10)
             dismissNotification()
-        case "60 second":
+        case "60 seconds":
             reminder(timeInterval: 60)
             dismissNotification()
         case "Dismiss":
@@ -66,42 +67,39 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
         }
     }
     
+    func setNotificationCategories() {
+        
+        let actions = [
+        UNNotificationAction(identifier: "Snooze", title: "Snooze", options: []),
+        UNNotificationAction(identifier: "Dismiss", title: "Dismiss", options: [.destructive])]
+        
+        let category = UNNotificationCategory(identifier: "User Actions",
+                                              actions: actions,
+                                              intentIdentifiers: [],
+                                              options: [])
+        
+        UNUserNotificationCenter.current().setNotificationCategories([category])
+    }
+    
     func reminder(timeInterval: Double) {
         
         let content = UNMutableNotificationContent()
         
         content.title = "Reminder"
         content.body = "Summer Time"
-        content.sound = UNNotificationSound.default
+        content.sound = .default
         content.categoryIdentifier = "User Actions"
         
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: false)
-        let uuid = UUID().uuidString
-        let request = UNNotificationRequest(identifier: uuid,
+        let request = UNNotificationRequest(identifier: "Local Notification",
                                             content: content,
                                             trigger: trigger)
         
         UNUserNotificationCenter.current().add(request) { (error) in
             if let error = error {
-                print("Error \(error.localizedDescription)")
+                print("Error \(error)")
             }
         }
     }
     
-    func setNotificationCategories() {
-        
-        let actions = [
-            UNNotificationAction(identifier: "Snooze",  title: "Snooze", options: []),
-            UNNotificationAction(identifier: "Dismiss",  title: "Dismiss", options: [.destructive]),
-            ]
-        
-        let category = UNNotificationCategory(identifier: "User Actions",
-                                              actions: actions,
-                                              intentIdentifiers: [],
-                                              hiddenPreviewsBodyPlaceholder: nil,
-                                              categorySummaryFormat: "%u новых уведомлений в разделе %@",
-                                              options: [])
-        
-        UNUserNotificationCenter.current().setNotificationCategories([category])
-    }
 }
